0c95a1c434c55cde8525c6290fdccdff8ad816b7
Per Ivar Gjerløw <pgjerlow@gmail.com>
2017-02-28 16:25:08 +0100

    Added getJiraIssueKeyFromCommitMessage method and implemented the convinience methods for checking overridden settings

--- src/main/java/org/karivar/utils/CommitMessageManipulator.java
+++ src/main/java/org/karivar/utils/CommitMessageManipulator.java
@@ -7,6 +7,9 @@
  */
 package org.karivar.utils;
 
+import com.google.common.base.Splitter;
+import com.google.common.base.Strings;
+import org.karivar.utils.domain.IssueKeyNotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,7 +18,8 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.stream.Collectors;
+import java.util.List;
+
 
 /**
  * This class is responsible for handling of the commit message. This includes reading, saving and extracting
@@ -28,15 +32,14 @@ public class CommitMessageManipulator {
     private static final String JIRA_ASSIGNEE_OVERRIDDEN = "-A";
     private static final String JIRA_COMMIT_OVERRIDDEN = "NONE";
 
-    private String commitFileContents = null;
+    private List<String> commitFileContents = null;
 
     public void loadCommitMessage(String filename) {
 
         // Load the commit message file
         Path path = Paths.get(filename);
         try {
-            commitFileContents = Files.lines(path, StandardCharsets.UTF_8)
-                    .collect(Collectors.joining());
+            commitFileContents = Files.readAllLines(path, StandardCharsets.UTF_8);
             logger.debug("The file contents are: \n\t {}", commitFileContents);
         } catch (IOException e) {
             logger.error("Unable to read commit message file. \nGot exception {}", e);
@@ -44,16 +47,85 @@ public class CommitMessageManipulator {
     }
 
     public boolean isCommunicationOverridden() {
-        return false;
+        boolean isOverridden = false;
+
+        String[] wordList = commitFileContents.get(0).split("\\s+");
+        String last = wordList[wordList.length - 1];
+
+        if (last.equalsIgnoreCase(JIRA_COMMUNICATION_OVERRIDDEN)) {
+            isOverridden = true;
+        }
+        return isOverridden;
     }
 
     public boolean isCommitOverridden() {
-        return false;
+
+        boolean isCommitOverridden = false;
+
+        String[] wordList = commitFileContents.get(0).split("\\s+");
+
+        if (wordList.length > 0) {
+            String first = wordList[0];
+
+            if (first.equals(JIRA_COMMIT_OVERRIDDEN)) {
+                isCommitOverridden = true;
+            }
+        }
+
+        return isCommitOverridden;
     }
 
     public boolean isAssigneeOverridden() {
-        return false;
+        boolean isAssigneeOverridden = false;
+
+        String[] wordList = commitFileContents.get(0).split("\\s+");
+        String last = wordList[wordList.length - 1];
+
+        if (last.equalsIgnoreCase(JIRA_ASSIGNEE_OVERRIDDEN)) {
+            isAssigneeOverridden = true;
+        }
+
+        return isAssigneeOverridden;
     }
 
+    public String getJiraIssueKeyFromCommitMessage(String jiraIssuePattern) throws IssueKeyNotFoundException {
+        String jiraIssueKey = null;
+        String firstLineOfCommitMessage = commitFileContents.get(0);
+        logger.debug("Starting getJiraIssueKeyFromCommitMessage({}, {})", firstLineOfCommitMessage, jiraIssuePattern);
 
+        if (!Strings.isNullOrEmpty(firstLineOfCommitMessage)) {
+
+            if (!Strings.isNullOrEmpty(jiraIssuePattern)) {
+                Splitter jiraPatternSplitter = Splitter.on(" ");
+                List<String> commitLineWords = jiraPatternSplitter.splitToList(firstLineOfCommitMessage);
+                List<String> jiraIssuePatterns = jiraPatternSplitter.splitToList(jiraIssuePattern);
+
+                if (!commitLineWords.isEmpty()) {
+                    for (String pattern : jiraIssuePatterns) {
+                        for (String word : commitLineWords) {
+                            if (word.toUpperCase().startsWith(pattern.toUpperCase())) {
+                                logger.debug("Found issue key {}", word);
+                                jiraIssueKey = word.toUpperCase();
+                                break;
+                            }
+                        }
+                    }
+                }  else{
+                    logger.error("The jira issue pattern is not found");
+                    throw new IssueKeyNotFoundException("The jira issue pattern is not found");
+                }
+            } else {
+                logger.error("The jira issue pattern is not found");
+                throw new IssueKeyNotFoundException("The jira issue pattern is not found");
+            }
+        }  else {
+                logger.error("The commit line is empty");
+                throw new IssueKeyNotFoundException("The commit line is empty");
+        }
+
+        return jiraIssueKey;
+    }
 }
+
+
+
---
+++ src/main/java/org/karivar/utils/domain/IssueKeyNotFoundException.java
@@ -0,0 +1,7 @@
+package org.karivar.utils.domain;
+
+public class IssueKeyNotFoundException extends Exception {
+    public IssueKeyNotFoundException(String message) {
+        super(message);
+    }
+}
