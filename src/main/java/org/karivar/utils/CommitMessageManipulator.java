37abdc803ddadd116081cc239d918f2a38c6603c
Per Ivar Gjerløw <pgjerlow@gmail.com>
2017-03-18 14:09:57 +0100

    Added method for adding traceability information according to the convention. Also added a method for writing the updated message

--- src/main/java/org/karivar/utils/CommitMessageManipulator.java
+++ src/main/java/org/karivar/utils/CommitMessageManipulator.java
@@ -7,16 +7,20 @@
  */
 package org.karivar.utils;
 
+import com.google.common.base.Charsets;
 import com.google.common.base.Splitter;
 import com.google.common.base.Strings;
+import com.google.common.io.FileWriteMode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+//import java.nio.charset.StandardCharsets;
+//import java.nio.file.Files;
+//import java.nio.file.Path;
+//import java.nio.file.Paths;
+import com.google.common.io.Files;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -34,18 +38,32 @@ public class CommitMessageManipulator {
     private static final String JIRA_ASSIGNEE_OVERRIDDEN = "-A";
     private static final String JIRA_COMMIT_OVERRIDDEN = "NONE";
 
+    private String commitMessageFilename;
     private List<String> commitFileContents = null;
     private boolean jiraIssueKeyFound;
+
 
     public void loadCommitMessage(String filename) {
 
         // Load the commit message file
-        Path path = Paths.get(filename);
+        //Path path = Paths.get(filename);
         try {
-            commitFileContents = Files.readAllLines(path, StandardCharsets.UTF_8);
+            //commitFileContents = Files.readAllLines(path, StandardCharsets.UTF_8);
+            File file = new File(filename);
+            commitFileContents = Files.readLines(file, Charsets.UTF_8);
             logger.debug("The file contents are: \n\t {}", commitFileContents);
+            commitMessageFilename = filename;
         } catch (IOException e) {
             logger.error("Unable to read commit message file. \nGot exception {}", e);
+        }
+    }
+
+    public void writeCommitMessage(List<String> commitFileContents) {
+        File file = new File(commitMessageFilename);
+        try {
+            Files.asCharSink(file, Charsets.UTF_8).writeLines(commitFileContents);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
@@ -127,13 +145,13 @@ public class CommitMessageManipulator {
     }
 
     /**
-     * Removes any options from the original commit message (first line)
+     * Removes any options from the original commit message (first line only)
      * @return
      */
-    public String getStrippedCommitMessage() {
+    public List<String> getStrippedCommitMessage() {
         ArrayList<String> strippedCommitMessage = (ArrayList<String>) commitFileContents;
         strippedCommitMessage.set(0, getStripped(strippedCommitMessage.get(0)));
-        return String.join(System.lineSeparator(), strippedCommitMessage);
+        return strippedCommitMessage;
 
     }
 
@@ -144,7 +162,7 @@ public class CommitMessageManipulator {
         List<String> arrayList = Arrays.asList(firstLine);
         String[] wordList = arrayList.get(0).split("\\s+");
 
-        if (isCommitOverridden() || jiraIssueKeyFound) {
+        if (isCommitOverridden() || isCommunicationOverridden() || jiraIssueKeyFound) {
             wordList[0] = wordList[0].toUpperCase();
         }
 
--- src/main/java/org/karivar/utils/GitHook.java
+++ src/main/java/org/karivar/utils/GitHook.java
@@ -10,6 +10,7 @@ package org.karivar.utils;
 import com.google.common.base.Splitter;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
+import org.karivar.utils.domain.BasicJiraIssue;
 import org.karivar.utils.domain.IssueKeyNotFoundException;
 import org.karivar.utils.domain.JiraIssue;
 import org.karivar.utils.other.UTF8Control;
@@ -21,6 +22,7 @@ import java.io.InputStream;
 import java.util.*;
 
 public class GitHook {
+    private static final String HOOK_VERSION = "0.0.1";
     private final Logger logger = LoggerFactory.getLogger(GitHook.class);
     private static GitHook githook;
     private ResourceBundle messages;
@@ -30,6 +32,9 @@ public class GitHook {
     private Map<String, List<String>> issueTypesAndStatuses;
     private List<String> issueLinks;
     private Optional<String> jiraProjectKeys;
+    private boolean commitOverridden = false;
+    private boolean jiraCommunicationOverridden = false;
+    private boolean assigneeOverridden = false;
 
     public static void main(String[] args) {
         githook = new GitHook();
@@ -47,7 +52,6 @@ public class GitHook {
             loadApplicationProperties();
             fetchPopulatedJiraIssue();
             checkStateAndManipulateCommitMessage();
-            updateCommitMessage();
 
         } else {
             logger.error(messages.getString("error.githook.nocommitfile"));
@@ -58,10 +62,10 @@ public class GitHook {
         // Get options for
         //   1: override communication with JIRA altogether
         //   2: override (e.g force) commits
-        boolean isJiraCommunicationOverridden = manipulator.isCommunicationOverridden();
-        boolean isCommitOverridden = manipulator.isCommitOverridden();
+        jiraCommunicationOverridden = manipulator.isCommunicationOverridden();
+        commitOverridden = manipulator.isCommitOverridden();
 
-        if (!isJiraCommunicationOverridden && !isCommitOverridden) {
+        if (!jiraCommunicationOverridden && !commitOverridden) {
             // Contact JIRA, fetch JIRA issue and check state and return populated issue
             logger.debug("Preparing to communicate with Jira");
 
@@ -75,6 +79,7 @@ public class GitHook {
                 populatedIssue = jiraConnector.getJiraPopulatedIssue(issueKey, issueLinks);
             } catch (IssueKeyNotFoundException e) {
                 logger.error(e.getLocalizedMessage());
+                System.exit(1);
             }
         } else {
             logger.debug("Communication with Jira is overridden or commit is overridden");
@@ -82,35 +87,141 @@ public class GitHook {
     }
 
     private void checkStateAndManipulateCommitMessage() {
-        //boolean statusOK;
         // check status against allowed statues
         boolean statusOK = checkAllowedStatus();
 
-        boolean assigneeOK =false;
-        if (!manipulator.isAssigneeOverridden()) {
+        boolean assigneeOK;
+        assigneeOverridden = manipulator.isAssigneeOverridden();
+        if (!assigneeOverridden) {
             // check assignee
             assigneeOK = checkAssignee();
+        } else if (manipulator.isCommitOverridden()) {
+            // the commit is overridden. Thus assignee is not found, but OK anyway
+            assigneeOK = true;
+        } else {
+            assigneeOK = assigneeOverridden;
         }
 
         if (statusOK && assigneeOK) {
             logger.debug("Status is OK");
 
-            String manipulatedMessage = manipulator.getStrippedCommitMessage();
-            logger.debug("The manipulated message is\n{}", manipulatedMessage);
+            List<String> manipulatedMessage = manipulator.getStrippedCommitMessage();
+            manipulatedMessage = addTraceabilityInformationToMessage(manipulatedMessage);
+            logger.debug("The manipulated message is {}", manipulatedMessage);
+            manipulator.writeCommitMessage(manipulatedMessage);
 
         } else {
             logger.debug("Status is not OK. ");
             if (!statusOK) {
-                logger.info(messages.getString("commitnotallowedstatus")
-                        + populatedIssue.getStatus());
-            }
+                if (populatedIssue != null) {
+                    logger.info(messages.getString("commitnotallowedstatus")
+                            + populatedIssue.getStatus());
+                }
 
-            if (!assigneeOK) {
-                logger.info(messages.getString("commitnotallowedassignee")
-                        + populatedIssue.getAssignee().get().getDisplayName());
+                if (!assigneeOK) {
+                    logger.info(messages.getString("commitnotallowedassignee")
+                            + populatedIssue.getAssignee().get().getDisplayName());
+                }
             }
         }
+    }
 
+    /**
+     * This method adds traceability information to the message according to the convention below
+     * <ORIGINAL_JIRAISSUE_KEY> <COMMIT MESSAGE><br>
+     *<empty line><br>
+     *summary: <JIRAISSUE_SUMMARY><br>
+     *sub-task of: <PARENT_JIRAISSUE_KEY> <PARENT__JIRAISSUE_SUMMARY> (optional)<br>
+     *related to: <RELATED_JIRAISSUE_KEY> <RELATED_JIRAISSUE_SUMMARY> (optional)<br>
+     *<empty line> (optional)<br>
+     *<additional information> (optional)<br>
+     *<hook version information><br>
+     * @param manipulatedMessage
+     * @return the message which conforms to the convention
+     */
+    private List<String> addTraceabilityInformationToMessage(final List<String> manipulatedMessage) {
+        ArrayList<String> addedTraceabilityMessage = (ArrayList<String>) manipulatedMessage;
+        addedTraceabilityMessage.add("");
+        String summaryInfo = getSummaryInformation();
+
+        if (summaryInfo !=null) {
+            addedTraceabilityMessage.add(summaryInfo);
+        }
+
+        if (populatedIssue != null && populatedIssue.isSubtask()) {
+            addedTraceabilityMessage.add(getParentIssueInformation());
+        }
+
+        List<String> relatedIssues = getRelatedIssuesInformation();
+        if (relatedIssues != null) {
+            addedTraceabilityMessage.addAll(relatedIssues);
+        }
+
+        List<String> additionalInformation = getAdditionalInformation();
+
+        if (additionalInformation.size() >= 1) {
+            addedTraceabilityMessage.addAll(additionalInformation);
+        }
+
+        addedTraceabilityMessage.add(getHookInformation());
+
+
+        return addedTraceabilityMessage;
+    }
+
+    private String getSummaryInformation() {
+        if (populatedIssue != null) {
+            return messages.getString("commit.convention.summary") + populatedIssue.getSummary();
+        }
+        return null;
+    }
+
+    private String getParentIssueInformation() {
+        if (populatedIssue.getParentIssue() != null && populatedIssue.getParentIssue().isPresent()) {
+            BasicJiraIssue parent = populatedIssue.getParentIssue().get();
+            return messages.getString("commit.convention.parentissue") + parent.getKey() + " " + parent.getSummary();
+        }
+        return null;
+    }
+
+    private List<String> getRelatedIssuesInformation() {
+        List<String>relatedIssueInformation = null;
+        if (populatedIssue != null &&
+                populatedIssue.getRelatedIssues() != null &&
+                populatedIssue.getRelatedIssues().size() > 0) {
+            Iterator<BasicJiraIssue> relatedIssuesIterator = populatedIssue.getRelatedIssues().iterator();
+            relatedIssueInformation = new ArrayList<>();
+            while (relatedIssuesIterator.hasNext()) {
+                BasicJiraIssue relatedIssue = relatedIssuesIterator.next();
+                String message = messages.getString("commit.convention.relatedissue") +
+                        relatedIssue.getKey() + " " + relatedIssue.getSummary();
+
+                if (relatedIssuesIterator.hasNext()) {
+                    message += System.lineSeparator();
+                }
+                relatedIssueInformation.add(message);
+            }
+        }
+        return relatedIssueInformation;
+    }
+
+    private List<String> getAdditionalInformation() {
+
+        List<String> additionalInfo = new ArrayList<>();
+
+        if (jiraCommunicationOverridden) {
+            additionalInfo.add(messages.getString("commit.convention.communicationoverridden"));
+        }
+
+        if (assigneeOverridden) {
+            additionalInfo.add(messages.getString("commit.convention.assigneeoverridden"));
+        }
+
+        return additionalInfo;
+    }
+
+    private String getHookInformation() {
+        return messages.getString("commit.convention.hookinformation") + HOOK_VERSION;
     }
 
     private boolean checkAssignee() {
@@ -134,6 +245,11 @@ public class GitHook {
             return true;
         }
 
+        if (manipulator.isCommitOverridden()){
+            // Have to handle if NONE as first parameter is used
+            return true;
+        }
+
         if (populatedIssue != null) {
             List<String> issueTypeStatuses = issueTypesAndStatuses.get(populatedIssue.getIssueTypeName());
 
@@ -153,7 +269,8 @@ public class GitHook {
         return false;
     }
 
-    private void updateCommitMessage() {
+    private void updateCommitMessage(List<String> manipulatedMessage) {
+        manipulator.writeCommitMessage(manipulatedMessage);
     }
 
     private void loadApplicationProperties() {
@@ -227,7 +344,7 @@ public class GitHook {
     }
 
     private void printInitalText() {
-        logger.info(messages.getString("startup.information") +  " 0.0.1");
+        logger.info(messages.getString("startup.information"), HOOK_VERSION);
     }
 
     private String getJiraIssueKey(Optional<String> jiraProjectPattern) {
